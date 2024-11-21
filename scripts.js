14e0c152921d046d4d37e13bde59dc1ae311067c
Ilya_Minin <mininiv2005@gmail.com>
2024-11-21 10:51:55 +0300

    1st commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const items = ["красный", "оранжевый", "желтый", "зеленый", "голубой","коричневый"];
+
+const vvod_add = document.getElementById("vvod_add");
+const vvod_search = document.getElementById("vvod_search")
+const vyvod = document.getElementById("vyvod");
+const add_submit = document.getElementById("add_item");
+const search_submit = document.getElementById("search_item");
+
+add_submit.addEventListener("click", () => {
+  items.push(vvod_add.value.toLowerCase());
+  vvod_add.value = "";
+});
+search_submit.addEventListener("click", () => {
+    vyvod.innerHTML = "";
+    items.forEach(item =>{
+        if(item.toLowerCase().includes(vvod_search.value.toLowerCase())) {
+            const li = document.createElement("li");
+            li.innerText = item;
+            vyvod.appendChild(li)
+        }else if(vvod_search.value == ''){
+            li.innerText = "Введите что-нибудь";
+        }
+    })
+    vvod_search.value = "";
+});
