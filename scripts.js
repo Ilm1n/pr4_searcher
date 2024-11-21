58a49ca87a8cd49d4ee55634ad8c1c48e934727b
Ilm1n <mininiv2005@gmail.com>
2024-11-21 11:16:33 +0300

    2nd commit

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
-const items = ["красный", "оранжевый", "желтый", "зеленый", "голубой","коричневый"];
+const items = [
+  "красный",
+  "оранжевый",
+  "желтый",
+  "зеленый",
+  "голубой",
+  "коричневый",
+];
 
 const vvod_add = document.getElementById("vvod_add");
-const vvod_search = document.getElementById("vvod_search")
+const vvod_search = document.getElementById("vvod_search");
 const vyvod = document.getElementById("vyvod");
 const add_submit = document.getElementById("add_item");
 const search_submit = document.getElementById("search_item");
@@ -11,15 +18,22 @@ add_submit.addEventListener("click", () => {
   vvod_add.value = "";
 });
 search_submit.addEventListener("click", () => {
-    vyvod.innerHTML = "";
-    items.forEach(item =>{
-        if(item.toLowerCase().includes(vvod_search.value.toLowerCase())) {
-            const li = document.createElement("li");
-            li.innerText = item;
-            vyvod.appendChild(li)
-        }else if(vvod_search.value == ''){
-            li.innerText = "Введите что-нибудь";
+  vyvod.innerHTML = "";
+  if (vvod_search.value == "") {
+    const p = document.createElement("p");
+    p.innerText = "Для поиска необходимо что-либо ввести";
+    p.style = "color:red"
+    vyvod.appendChild(p);
+  }
+  else {
+    items.forEach((item) => {
+        if (item.toLowerCase().includes(vvod_search.value.toLowerCase())) {
+          const li = document.createElement("li");
+          li.innerText = item;
+          vyvod.appendChild(li);
         }
-    })
-    vvod_search.value = "";
+      });
+  }
+  
+  vvod_search.value = "";
 });
